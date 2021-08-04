b14cf727ee78caf3404c5b2ecc77990e3916dd81
OllieDenney <18odenney@wakatipu.school.nz>
2021-08-05 09:59:17 +1200

    5/8/21 - Began to develop a Javascript animation for the dropdown nav

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
   function showDiv() {
     var x = document.getElementById("dropdown");
+    var y = document.getElementById("nav-tab")
     if (x.style.display === "none") {
       x.style.display = "block";
+      y.
     } else {
       x.style.display = "none";
     }
