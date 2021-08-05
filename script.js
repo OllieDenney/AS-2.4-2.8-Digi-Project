7f45e1adef3c570e633518b104bb89b1a9c22aa0
OllieDenney <18odenney@wakatipu.school.nz>
2021-08-05 13:49:20 +1200

    5/8/21 - Added some content to the page, and added a placeholder image.

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-  function showDiv() {
-    var x = document.getElementById("dropdown");
-    var y = document.getElementById("nav-tab")
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      y.
-    } else {
-      x.style.display = "none";
-    }
+
+function showDiv() {
+  var x = document.getElementById("dropdown");
+  if (x.style.display === "none") {
+    x.style.display = "block";
   }
+  else {
+    x.style.display = "none";
+  }
+}
+/*
+
+$(function showDiv() {
+    $('#dropdown').addClass('hidden').hide();
+    $('#').click(function() {
+        if ($('#dropdown').hasClass('hidden')) {
+            $('#dropdown').removeClass('hidden').fadeIn(1000);
+        }
+        else {
+            $('#dropdown').addClass('hidden').fadeOut(1000);
+        }
+    });
+});
+
+
+function showDiv(){
+  $("#nav-tab").fadeIn();
+});
+*/
