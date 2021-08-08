c3b1fbe424cf970e8c3f11771e8d175ba475760d
OllieDenney <18odenney@wakatipu.school.nz>
2021-08-09 11:44:33 +1200

    8/8/21 Rearranged home page and further developed the nav bar by beginning to add social media contacts

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 function showDiv() {
   var x = document.getElementById("dropdown");
+  var y = document.getElementById("dropdown-menu");
   if (x.style.display === "none") {
     x.style.display = "block";
+    y.style.marginTop = "2.9%";
   }
   else {
     x.style.display = "none";
+    y.style.marginTop = "0%";
   }
 }
 /*
@@ -27,3 +30,16 @@ function showDiv(){
   $("#nav-tab").fadeIn();
 });
 */
+
+document.querySelector(document).ready(function(){
+    document.querySelector('.up-down').mouseover(function(){
+        document.querySelector('.default').stop().animate({
+            height: 0
+        }, 200);
+    }).mouseout(function(){
+        document.querySelector('.default').stop().animate({
+            height: 200
+        }, 200)
+    })
+
+});
